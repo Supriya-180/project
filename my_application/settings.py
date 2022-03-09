0d443f6d287a9a287acf2c52792756fc6fa284c8
Supriya-180 <supriyagkp206@gmail.com>
2022-03-10 01:15:41 +0800

    allowed host

--- my_application/settings.py
+++ my_application/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-i2in0jozzmbs8#wj8=e6-bfrsqu2np$5)7r_5qo%ufy*)-*@%f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['supriya206.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -142,3 +143,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
